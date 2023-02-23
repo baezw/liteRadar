a33909883869d4daff5d22f569a8a60fd871128e
billy27607 <billy@baezw.com>
2023-02-23 14:14:14 -0500

    add built-in example

--- examples/built_in_scenarios/built_in_scenarios.ino
+++ examples/built_in_scenarios/built_in_scenarios.ino
@@ -1,13 +1,10 @@
 
 
 /*!
- * This is example of using the library in "custom" mode to set presence threshold anf motion threshold levels
+ * This is example of using the modules built in scenarios
  *
  * One will need to appropriately modify the second Serial refernce - here it is Serial 2 -
  * in order to make it work on the board you are working with.
- * 
- * I have found this mode to be much more useful an flexible in tuning the sensor for 
- * a particular area as opposed to the built in scenarios
  * 
  */
 
@@ -31,13 +28,12 @@ void setup() {
     if (!radar.resetRadar()) Serial.println("reset failed");
     delay(1000);
 
-    if(!radar.openCustomMode(MODE_1)) Serial.println("open custom mode failed");
+    if (!radar.setScenario(AREA_DETECTION)) Serial.println("set scenario failed");
     delay(1000);
-    if (!radar.setPresenceThreshold(0x1E)) Serial.println("set presence threshold failed");
+    if (!radar.setSensitivity(0x02)) Serial.println("set sensitivity failed");
     delay(1000);
-    if (!radar.setMotionThreshold(0x0E)) Serial.println("set presence threshold failed");
-    delay(1000);
-    if(!radar.exitCustomMode()) Serial.println("exit custom mode failed");
+
+    if (!radar.resetRadar()) Serial.println("reset failed");
     delay(1000);
 
     Serial.println("\nsetup done\n\n\n");
